7b11cf0ff8678431b32e5d966c79d12fe5c2969e
SakibHasanCse <sakibhasan9641@gmail.com>
2021-08-20 23:55:34 +0600

    first auction

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,16 @@
+import { v4 as uuid } from 'uuid';
+
 async function createAuction(event, context) {
+  const { title, description } = JSON.parse(event.body);
+
+  const date = new Date().toISOString();
+  const auction = {
+    id: uuid(),
+    title, description, date
+  }
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    statusCode: 201,
+    body: JSON.stringify(auction)
   };
 }
 
